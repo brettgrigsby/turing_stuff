8aa249447a881f0dc3a4db94b37f99bcd679fb6a
brettgrigsby <gbrett@gmail.com>
2015-04-04 14:54:32 -0600

    Update raindrops.rb

--- raindrops.rb
+++ raindrops.rb
@@ -2,9 +2,9 @@ require 'Prime'
 
 class Raindrops
   def self.convert(number)
-
-    drop_speak = []
+    drop_speak = [] #an array to hold our sounds
     has_primes = 0 #keeps track of whether our number has prime factors
+    
     factors = Prime.prime_division(number) #finds prime factors and puts them into an array of 2-element arrays
 
     if factors.flatten.include?(3) #we flatten so the array of arrays becomes just 1 array that .include? can search
