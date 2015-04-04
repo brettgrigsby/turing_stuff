ec6b1b7d950a6222f0afed6c3f15372ed168569c
brettgrigsby <gbrett@gmail.com>
2015-04-04 14:53:14 -0600

    Update fibber.rb

--- fibber.rb
+++ fibber.rb
@@ -1,11 +1,7 @@
+puts "Enter sequnce length:"
 quantity = gets.to_i
-
 sequence = [ 0, 1]
 
-(quantity - 2).times do
-  sequence << sequence[-2] + sequence[-1]
-end
+(quantity - 2).times { sequence << sequence[-2] + sequence[-1] }
 
-sequence.each_slice(5) do |row|
-  puts row.map { |e| "%#{sequence[-1].to_s.length}d" % e }.join("  ")
-end
+sequence.each_slice(5) { |row| puts row.map { |e| "%#{sequence[-1].to_s.length + 1}d" % e }.join }
