7756ded985be2a284173bc20e144ce62ac7291c7
Brett Grigsby <gbrett@gmail.com>
2015-04-06 09:46:31 -0600

    Update fibber.rb

--- fibber.rb
+++ fibber.rb
@@ -4,4 +4,4 @@ sequence = [ 0, 1]
 
 (quantity - 2).times { sequence << sequence[-2] + sequence[-1] }
 
-sequence.each_slice(5) { |row| puts row.map { |e| "%#{sequence[-1].to_s.length + 1}d" % e }.join }
+sequence.each_slice(5) { |row| puts row.map { |num| "%#{sequence[-1].to_s.length + 1}d" % num }.join }
