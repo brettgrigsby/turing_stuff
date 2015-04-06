48f7a9d2ed403a05ecea773f77c4a52a9d726b1b
Brett Grigsby <gbrett@gmail.com>
2015-04-05 23:33:54 -0600

    Update roman_numerals.rb

--- roman_numerals.rb
+++ roman_numerals.rb
@@ -3,26 +3,29 @@ class Fixnum
   def to_roman
 
     output = []
-    let_key = 1
-    letters = {0 => "I", 1 => "I", 2 => "V", 3 => "X", 4 => "L", 5 => "C", 6 => "D", 7 => "M"}
-    size = self.to_s.length
+    letters = {2 => "I", 3 => "V", 4 => "X", 5 => "L", 6 => "C", 7 => "D", 8 => "M"}
+    let_key = self.to_s.length * 2
     num_set = self.to_s.chars.map(&:to_i)
-    remainder = num_set[0] % 5
 
+    num_set.each do |x|
+      remainder = x % 5
 
-    if num_set[0] < 4
-      remainder.times { output << letters[let_key] }
-    elsif num_set[0] == 4
-      output << letters[let_key]
-      output << letters[let_key + 1]
-    elsif num_set[0] == 5
-      output << letters[let_key + 1]
-    elsif num_set[0] > 5 && num_set[0] != 9
-      output << letters[let_key + 1]
-      remainder.times { output << letters[let_key] }
-    else
-      output << letters[let_key]
-      output << letters[let_key + 2]
+      if x < 4
+        remainder.times { output << letters[let_key] }
+      elsif x == 4
+        output << letters[let_key]
+        output << letters[let_key + 1]
+      elsif x == 5
+        output << letters[let_key + 1]
+      elsif x > 5 && x != 9
+        output << letters[let_key + 1]
+        remainder.times { output << letters[let_key] }
+      else
+        output << letters[let_key]
+        output << letters[let_key + 2]
+      end
+
+      let_key -= 2
     end
 
     output.join
